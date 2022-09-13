f755981c358618305a2cf1f52a741a8602f7c12c
Lauro <laurowrn@gmail.com>
2022-09-13 10:41:52 -0300

    Revert "Retirada de ponto e virgula"
    This reverts commit 658251eec15c6e7784b883b4d8eee76f120f989b.

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int ret
-    ret = soma(2, 3)
-    printf("O resultado é: %d", ret)
-    return 0
+    int ret;
+    ret = soma(2, 3);
+    printf("O resultado é: %d", ret);
+    return 0;
 }
